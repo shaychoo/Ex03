55d15c845016ef37afb9c56528b234fc9f1de9c2
netanels <netanel.s@gmail.com>
2014-12-19 12:33:22 +0200

    Add basic entities

---
+++ A15 Ex01 Shay 200779072 Nethanel 301294898/Ex03.GarageLogic/Entities/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic.Entities
+{
+    public class Wheel
+    {
+        public string ManufacturerName { get; set; }
+
+        public float CurrentAirPressure { get; set; }
+
+        public float MaximumAirPressure { get; set; }
+
+        public void Inflate(float i_Amount)
+        {
+            //todo
+        }
+    }
+}
